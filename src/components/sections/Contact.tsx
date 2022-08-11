1c924c583e8685e0842d362c3a912ec9d223da3f
noahcorona <noahcorona@gmail.com>
2022-08-11 00:04:08 -0700

    add emailjs, set up contact form

--- src/components/sections/Contact.tsx
+++ src/components/sections/Contact.tsx
@@ -1,12 +1,15 @@
-import React, {useState} from 'react';
+import React, {useRef, useState} from 'react';
 import {Button, Form} from 'react-bootstrap';
 import '../../style/App.css';
 import '../../style/Contact.css';
+import emailCredentials from '../../constants/emailCredentials';
+import emailjs from '@emailjs/browser';
 
 const CONTACT_EMAIL = 'noahcorona@gmail.com';
 
 const Contact = () => {
   const [status, setStatus] = useState('Send');
+  const form = useRef<any>();
 
   /**
    * to-do: function that sends contain info to the requested email
@@ -16,6 +19,18 @@ const Contact = () => {
     e.preventDefault();
     setStatus('Sent');
     console.log(status, CONTACT_EMAIL);
+
+    emailjs.sendForm(
+        emailCredentials.SERVICE_ID,
+        emailCredentials.TEMPLATE_ID,
+        form.current,
+        '4PzI-riqR7fAvxPnW',
+    )
+        .then((result) => {
+          console.log(result.text);
+        }, (error) => {
+          console.log(error.text);
+        });
   }
 
   return (
@@ -27,22 +42,25 @@ const Contact = () => {
         <div className="decorated bottom-spaced">
           <span><h1>Feedback & Inquiries</h1></span>
         </div>
-        <Form onSubmit={handleSubmit}>
+        <Form
+          ref={form}
+          onSubmit={handleSubmit}
+        >
           <div className="d-flex gap-3 bottom-spaced">
             <Form.Control
-              id="name"
+              name="name"
               type="text"
               placeholder="Name *"
               required
             />
             <Form.Control
-              id="email"
+              name="email"
               type="email"
               placeholder="Email address *"
               required
             />
             <Form.Control
-              id="company"
+              name="company"
               type="text"
               placeholder="Company"
             />
@@ -50,7 +68,7 @@ const Contact = () => {
           <Form.Control
             className="bottom-spaced"
             as="textarea"
-            id="message"
+            name="message"
             type="text"
             placeholder="Your feedback is greatly appreciated!"
             required
