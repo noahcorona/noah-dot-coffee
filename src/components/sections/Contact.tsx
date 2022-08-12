b6435c2b01df9f40f1e792debbbcd711c030314e
noahcorona <noahcorona@gmail.com>
2022-08-12 11:37:15 -0700

    improve contact form

--- src/components/sections/Contact.tsx
+++ src/components/sections/Contact.tsx
@@ -1,25 +1,26 @@
 import React, {useRef, useState} from 'react';
 import {Button, Form} from 'react-bootstrap';
-import '../../style/App.css';
-import '../../style/Contact.css';
 import emailCredentials from '../../constants/emailCredentials';
 import emailjs from '@emailjs/browser';
-
-const CONTACT_EMAIL = 'noahcorona@gmail.com';
+import '../../style/App.css';
+import '../../style/Contact.css';
+// eslint-disable-next-line max-len
+import {AiOutlineArrowRight} from '@react-icons/all-files/ai/AiOutlineArrowRight';
 
 const Contact = () => {
   const [status, setStatus] = useState('Send');
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [company, setCompany] = useState('');
+  const [message, setMessage] = useState('');
   const form = useRef<any>();
 
   /**
-   * to-do: function that sends contain info to the requested email
-   * @param {event} e
+   *
+   * @param {event} e the form submission event
    */
   async function handleSubmit(e: any) {
     e.preventDefault();
-    setStatus('Sent');
-    console.log(status, CONTACT_EMAIL);
-
     emailjs.sendForm(
         emailCredentials.SERVICE_ID,
         emailCredentials.TEMPLATE_ID,
@@ -27,8 +28,10 @@ const Contact = () => {
         '4PzI-riqR7fAvxPnW',
     )
         .then((result) => {
-          console.log(result.text);
+          setStatus('Sent');
+          console.log(e.target);
         }, (error) => {
+          setStatus(error.text);
           console.log(error.text);
         });
   }
@@ -42,41 +45,85 @@ const Contact = () => {
         <div className="decorated bottom-spaced">
           <span><h1>Feedback & Inquiries</h1></span>
         </div>
-        <Form
-          ref={form}
-          onSubmit={handleSubmit}
-        >
-          <div className="d-flex gap-3 bottom-spaced">
-            <Form.Control
-              name="name"
-              type="text"
-              placeholder="Name *"
-              required
-            />
-            <Form.Control
-              name="email"
-              type="email"
-              placeholder="Email address *"
-              required
-            />
-            <Form.Control
-              name="company"
-              type="text"
-              placeholder="Company"
-            />
-          </div>
-          <Form.Control
-            className="bottom-spaced"
-            as="textarea"
-            name="message"
-            type="text"
-            placeholder="Your feedback is greatly appreciated!"
-            required
-          />
-          <Button type="submit">
-            {status}
-          </Button>
-        </Form>
+        {
+          status === 'Sent' ? (
+              <>
+                <h4>
+                  {'Thanks for reaching out!'}
+                  <br /><br />
+                  {'Here\'s a copy of your message:'}
+                  <br />
+                </h4>
+                <h5><u>Name</u>{' ' + name}</h5>
+                {email ? (
+                  <>
+                    <h5><u>E-mail Address</u>{' ' + email}</h5>
+                  </>
+                ) : <div />}
+                {company ? (
+                  <>
+                    <h5><u>Company</u>{' ' + company}</h5>
+                  </>
+                ) : <div />}
+                <h5><u>Body</u><p>{message}</p></h5>
+              </>
+          ) : (
+              <Form
+                ref={form}
+                onSubmit={handleSubmit}
+              >
+                <div className="d-flex gap-3 bottom-spaced">
+                  <Form.Control
+                    name="name"
+                    id="name"
+                    type="text"
+                    placeholder="Name *"
+                    onChange={(event) => setName(event.target.value)}
+                    required
+                  />
+                  <Form.Control
+                    name="email"
+                    id="email"
+                    type="email"
+                    onChange={(event) => setEmail(event.target.value)}
+                    placeholder="Email address"
+                  />
+                  <Form.Control
+                    name="company"
+                    id="company"
+                    type="text"
+                    onChange={(event) => setCompany(event.target.value)}
+                    placeholder="Company"
+                  />
+                </div>
+                <Form.Control
+                  className="bottom-spaced"
+                  as="textarea"
+                  rows={4}
+                  name="message"
+                  id="message"
+                  type="text"
+                  onChange={(event) => setMessage(event.target.value)}
+                  placeholder="Message body"
+                  required
+                />
+                <Button
+                  type="submit"
+                  bsPrefix="Contact-Form-Submit-Button"
+                >
+                  {'Send  '}
+                  <AiOutlineArrowRight />
+                </Button>
+                {
+                  status !== 'Send' && (
+                    <>
+                      <h4>Error</h4>
+                      <p>{status}</p>
+                    </>)
+                }
+              </Form>
+          )
+        }
       </div>
     </div>
   );
